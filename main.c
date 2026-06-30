715a5631e3a722bd9e815837febde18f95c9c491
eduardoa2025-cyber <eduardoa.2025@alunos.utfpr.edu.br>
2026-06-29 21:02:31 -0300

    #BNVEMM - iniciando-a-matriz - Escondendo navio inimigo.
    Foi adicionado a alteração que visa a não visualização do navio do oponente.
    
    Co-Authored-By: Mateus Do Prado Alcântara <118356038+Matz2022@users.noreply.github.com>

--- main.c
+++ main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 
 #define AGUA 0
 #define AGUADESCONHECIDA 7
 #define TAMANHO 8
 #define NAVIO 1
 #define NAVIOATINGIDO 2
+#define TAMANHO_NAVIO 2  
+#define QTD_NAVIOS 4 
+
+typedef struct {
+    int linha[TAMANHO_NAVIO];
+    int coluna[TAMANHO_NAVIO];
+    int partesAtingidas;
+    int afundado;
+} Navio;
+
+int verificaSobreposicao(int **campo, int tamanho, int linha, int coluna) {
+    if (linha < 0 || linha >= tamanho || coluna < 0 || coluna >= tamanho)
+        return 1;
+
+    return campo[linha][coluna] != AGUA;
+}
 
 void limpar_campo(int **campo, int tamanho){
     if(campo != NULL){
@@ -62,7 +78,42 @@ void mostrarmatriz(int **matriz, int tam, const char *titulo){
     }
 }
 
+void posicionarNaviosAleatorio(int **campo, Navio *navios, int tamanho, int qtd_navios) {
+    for (int i = 0; i < qtd_navios; i++) {
+        int posicionado = 0;
+
+        while (!posicionado) {
+            int orientacao = rand() % 2;
+
+            int lin1 = rand() % tamanho;
+            int col1 = rand() % tamanho;
+
+            int lin2 = lin1 + (orientacao == 1 ? 1 : 0);
+            int col2 = col1 + (orientacao == 0 ? 1 : 0);
+
+            if (lin2 < tamanho && col2 < tamanho) {
+                if (!verificaSobreposicao(campo, tamanho, lin1, col1) &&
+                    !verificaSobreposicao(campo, tamanho, lin2, col2)) {
+
+                    navios[i].linha[0] = lin1;
+                    navios[i].coluna[0] = col1;
+                    navios[i].linha[1] = lin2;
+                    navios[i].coluna[1] = col2;
+                    navios[i].partesAtingidas = 0;
+                    navios[i].afundado = 0;
+
+                    campo[lin1][col1] = NAVIO;
+                    campo[lin2][col2] = NAVIO;
+
+                    posicionado = 1;
+                }
+            }
+        }
+    }
+}
+
 int main(){
+    srand((unsigned int)time(NULL));
 
     printf("1 = Novo Jogo\n");
     printf("2 = Configurar\n");
@@ -72,61 +123,64 @@ int main(){
     int n;
     int **seu_campo = NULL;
     int **campo_visao_adversario = NULL;
+    int **campo_computador = NULL;
+    int **visao_computador = NULL;
 
     scanf("%d", &n);
 
     switch(n){
 
         case 1:
-
             printf("Novo Jogo\n");
 
             seu_campo = AlocarMatriz(TAMANHO);
             campo_visao_adversario = AlocarMatriz(TAMANHO);
+            campo_computador = AlocarMatriz(TAMANHO);
+            visao_computador = AlocarMatriz(TAMANHO);
 
-            if(campo_visao_adversario != NULL){
+            Navio navios_jogador[QTD_NAVIOS];
+            Navio navios_computador[QTD_NAVIOS];
+            
+            if(campo_visao_adversario != NULL && visao_computador != NULL){
                 for(int i = 0; i < TAMANHO; i++){
                     for(int j = 0; j < TAMANHO; j++){
                         campo_visao_adversario[i][j] = AGUADESCONHECIDA;
+                        visao_computador[i][j] = AGUADESCONHECIDA;
                     }
                 }
             }
 
-            if(seu_campo != NULL && campo_visao_adversario != NULL){
+            if(seu_campo != NULL && campo_visao_adversario != NULL && campo_computador != NULL && visao_computador != NULL){
+                
+                posicionarNaviosAleatorio(seu_campo, navios_jogador, TAMANHO, QTD_NAVIOS);
+                posicionarNaviosAleatorio(campo_computador, navios_computador, TAMANHO, QTD_NAVIOS);
 
-                printf("\nMatrizes alocadas com sucesso!\n");
+                printf("\nMatrizes alocadas e navios de ambos posicionados com sucesso!\n");
 
-                mostrarmatriz(seu_campo, TAMANHO, "SEU CAMPO");
-                mostrarmatriz(campo_visao_adversario, TAMANHO, "CAMPO DE VISAO DO ADVERSARIO");
+                mostrarmatriz(seu_campo, TAMANHO, "SEU CAMPO (NAVIOS = 1)");
+                mostrarmatriz(campo_visao_adversario, TAMANHO, "CAMPO DO ADVERSARIO (ESCONDIDO)");
 
                 limpar_campo(seu_campo, TAMANHO);
                 limpar_campo(campo_visao_adversario, TAMANHO);
+                limpar_campo(campo_computador, TAMANHO);
+                limpar_campo(visao_computador, TAMANHO);
             }
-
             break;
 
         case 2:
-
             printf("Configurar\n");
-
             break;
 
         case 3:
-
             printf("Carregar ultimo jogo\n");
-
             break;
 
         case 4:
-
             printf("Sair\n");
-
             break;
 
         default:
-
             printf("Nenhuma opcao encontrada.\n");
-
             break;
     }
 
